d5876528d233a7e788eff67669296bb5a4104874
Brian <brmurr@gmail.com>
2024-12-19 18:30:04 -0500

    Update unifi_setup.sh

--- unifi_setup.sh
+++ unifi_setup.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-#install script for unifi controller 5.13.29
+# Unifi Controller Install Script
+# Brian Murray
+# Version 2
 
 apt update && sudo apt upgrade -y
 apt install -y net-tools
-#helps with entropy issues for vm and headless servers
+apt install gnupg curl
+# helps with entropy issues for vm and headless servers
 apt-get install -y haveged
 update-rc.d haveged defaults
-#Unifi requires java 8 (newer versions may break controller)
-apt install -y openjdk-8-jre-headless
-#stops java 11 from being installed automatically (newer versions may break controller)
-apt-mark hold openjdk-11-*
+
 apt install -y ca-certificates apt-transport-https
-#add Unifi software repo requirements
-wget -qO /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
-echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-#add mongo database software repo requirements
-wget -qO - https://www.mongodb.org/static/pgp/server-3.6.asc | sudo apt-key add -
-echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-#dependacy for mongo 3.*
-curl -O http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
-apt install -y ./libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+# add Unifi software repo requirements
+wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
+echo 'deb [ arch=amd64,arm64 ] https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+
+# add mongo database software repo requirements
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | >
+
 apt update
+
+# install Mondgodb
 apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
+
+# install unifi controller
 apt install -y unifi
